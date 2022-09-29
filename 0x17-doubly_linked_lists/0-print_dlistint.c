8454537b5bf7bfe9aba8bf86a349f8c1aaa3659d
israel-1947 <israeldereje19@gmail.com>
2022-09-29 08:37:44 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
- *
- * @h: the head pointer to the list
- *
- * Return: number of nodes in the list
+ * print_dlistint - prints data in nodes
+ * @h: head of node
+ * Return: how many nodes
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
+		count++;
 	}
+
 	return (count);
 }
